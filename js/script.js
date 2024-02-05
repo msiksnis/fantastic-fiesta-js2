e13c083d72cab70ca0ed8027860fd165cc03f086
Martins Siksnis <msiksnis@gmail.com>
2024-02-05 22:41:42 +0100

    small typo fix

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 async function fetchAndLogPosts() {
-  const accessToken = localStorage.getItem("accessToken"); // Retrieve the access token from local storage
+  const accessToken = localStorage.getItem("accessToken");
 
   if (!accessToken) {
     console.log("No access token found. Please login.");
@@ -7,29 +7,24 @@ async function fetchAndLogPosts() {
   }
 
   try {
-    const response = await fetch(
-      "https://api.noroff.dev/api/v1/social/profiles",
-      {
-        // Replace with the actual endpoint
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-          // If your API requires an API key, include it as well
-          // "X-Noroff-API-Key": "your-api-key-here", // Replace with your actual API key
-        },
-      }
-    );
+    const response = await fetch("https://api.noroff.dev/api/v1/social/posts", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        // API key here
+        // "X-Noroff-API-Key": "api-key-here",
+      },
+    });
 
     if (!response.ok) {
       throw new Error(`Failed to fetch posts: ${response.statusText}`);
     }
 
     const posts = await response.json();
-    console.log(posts); // Log the posts to the console
+    console.log(posts);
   } catch (error) {
     console.error("Error fetching posts:", error);
   }
 }
 
-// Execute the function when the script loads
 fetchAndLogPosts();
