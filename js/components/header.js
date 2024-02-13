60740b3fb3d3d57e977dbad8889f3438713a4aed
Martins Siksnis <msiksnis@gmail.com>
2024-02-13 23:11:54 +0100

    added logout functionality

--- js/components/header.js
+++ js/components/header.js
@@ -1,3 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("../../components/header.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("header-container").innerHTML = data;
+      setActiveLink();
+      logout();
+      addProfileToggle();
+    });
+});
+
 function setActiveLink() {
   const icons = document.querySelectorAll(".active-icon");
   icons.forEach((icon) => {
@@ -10,11 +21,26 @@ function setActiveLink() {
   });
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  fetch("../../components/header.html")
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById("header-container").innerHTML = data;
-      setActiveLink();
+function addProfileToggle() {
+  const profileContainer = document.getElementById("profile-container");
+  const logoutText = document.getElementById("log-out-text");
+
+  profileContainer.addEventListener("click", () => {
+    profileContainer.classList.toggle("w-1/3");
+    logoutText.classList.toggle("group-hover:flex");
+
+    logoutText.addEventListener("click", () => {
+      localStorage.removeItem("accessToken");
+      window.location.href = "/";
     });
-});
+  });
+}
+
+function logout() {
+  const logoutTextDesktop = document.getElementById("log-out-text-desktop");
+
+  logoutTextDesktop.addEventListener("click", () => {
+    localStorage.removeItem("accessToken");
+    window.location.href = "/";
+  });
+}
